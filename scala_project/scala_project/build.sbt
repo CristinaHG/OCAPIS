81344b42ac71aa5e357a483b6d759c71bc407293
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-06-26 20:08:16 +0200

    svmop

---
+++ scala_project/scala_project/build.sbt
@@ -0,0 +1,5 @@
+name := "alvin2"
+
+version := "0.1"
+
+scalaVersion := "2.11.11"
---
+++ scala_project/scala_project/src/main/scala/Hello.scala
@@ -0,0 +1,16 @@
+package cristinahg.ocapis
+
+private class svmop(val cost:Double=0.1,val gamma:Double=0.1, var weights:Boolean=true){
+  var svmweights=Array[Double]()
+
+  //def fitsvm()
+
+  def computeWeights(p:Int,targets:Array[Int]):Array[Double]={
+    targets.foreach(i=> if(i<=p) svmweights.update(targets.indexOf(i),p+1-targets(targets.indexOf(i))) * targets.filter(_<=p).length / )
+  }
+}
+object svmop{
+
+//  def givenumber:Int=
+//  return 12
+}
