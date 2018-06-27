4926e82214046dc1d61712b62b85a79a364efce6
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-06-27 11:45:16 +0200

    adding test to check basic functionality

--- ocapis_source/build.sbt
+++ ocapis_source/build.sbt
@@ -3,3 +3,5 @@ name := "ocapis_source"
 version := "0.1"
 
 scalaVersion := "2.11.11"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.12" % "3.0.5" % "test"
--- ocapis_source/src/main/scala/svmop.scala
+++ ocapis_source/src/main/scala/svmop.scala
@@ -1,4 +1,4 @@
-// package cristinahg.ocapis
+package cristinahg.ocapis
 
 class svmop(val cost:Double=1.0,val gamma:Double=0.1, var weights:Boolean=true){
   var svmweights=Array[Double]()
@@ -6,11 +6,9 @@ class svmop(val cost:Double=1.0,val gamma:Double=0.1, var weights:Boolean=true){
   //def fitsvm()
 
   def computeWeights(p:Int,targets:Array[Int]):Array[Double]={
-    //targets.foreach(i=> if(i<=p) svmweights.update(targets.indexOf(i),p+1-targets(targets.indexOf(i))) * targets.filter(_<=p).length / )
     val weightscomputed=targets.map(i=>
           if(i<=p) (p + 1 - i*1.0) * (targets.filter(_ <= p).length ) / (targets.filter(_ <= p).map(j=> p+1-j).sum)
-          else (i*1.0-p) * (targets.filter(_> p).length) / (targets.filter(_> p).map(j=>j-p).sum)
-        )
+          else (i*1.0-p) * (targets.filter(_> p).length) / (targets.filter(_> p).map(j=>j-p).sum))
     return weightscomputed
   }
 }
