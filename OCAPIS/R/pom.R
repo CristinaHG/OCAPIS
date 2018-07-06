d866f49b86eee3a20e2be066c5f2554c0ab7b92b
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-07-06 18:37:47 +0200

    making columns names equal for predict in pompredict

--- OCAPIS/R/pom.R
+++ OCAPIS/R/pom.R
@@ -5,19 +5,24 @@ pomfit<-function(train,trainLabels,linkfunction="logistic"){
   setlinkfunc<-function(value){
     if(!(value %in% c("logistic","probit","log-log","cauchit"))) stop("possible linkfunctions are: 'logistic','probit','loglog','cloglog' or 'cauchit'")
   }
+  if(!is.matrix(train)){
+    train<-as.matrix(train)
+  }
   setlinkfunc(linkfunction)
   fit<-polr(trainLabels~.,data=train,Hess = TRUE,method = linkfunction)
-  #thresholds=-fit$zeta
-  #projection<-fit$coefficients
   fit
 }
 
 # fit<-polr(as.factor(dattrain[,ncol(dattrain)])~.,data=dattrain[,-ncol(dattrain)],Hess = TRUE,method ="logistic")
-#myfit<-pomfit(dattrain[,-ncol(dattrain)],as.factor(dattrain[,ncol(dattrain)]),"logistic")
+# fit<-pomfit(dattrain[,-ncol(dattrain)],as.factor(dattrain[,ncol(dattrain)]),"logistic")
 
 pompredict<-function(model,test){
+  names(test)<-names(model$coefficients)
+  preds<-predict(model,test,type="probs")
   predicted<-as.numeric(apply(model$fitted.values,1,which.max))
   projected<-fit$coefficients*test
   c(projected,predicted)
 }
-fit$coefficients*dattest[,-ncol(dattest)]
+
+#fit$coefficients*dattest[,-ncol(dattest)]
+#identical(predict(fit,dattrain[,-ncol(dattrain)],type="probs"),fit$fitted.values)
