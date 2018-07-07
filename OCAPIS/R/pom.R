b91ec2a6c1cfc9865eef2fdd98526dc392efa685
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-07-07 12:52:08 +0200

    adding doc for ponfit

--- OCAPIS/R/pom.R
+++ OCAPIS/R/pom.R
@@ -1,19 +1,25 @@
 
-
+#' Train a proportional odd model for ordinal regression
+#'
+#' train data must be the training data without labels. Labels should be provided in trainLabels.
+#' @param x Training data of numeric type without labels.
+#' @param y Tags for each instance of training data. Must be factors.
+#' @param linkfunction link function to be used in the ordinal logistic regression fit. Possible functions are: logistic','probit','loglog','cloglog' or 'cauchit'.
+#' @return the fitted model.
+#' @examples
+#' dattrain<-read.csv("train_balance-scale.0", sep=" ")
+#' fit<-pomfit(dattrain[,-ncol(dattrain)],as.factor(dattrain[,ncol(dattrain)]),"logistic")
+#'
 
 pomfit<-function(train,trainLabels,linkfunction="logistic"){
   setlinkfunc<-function(value){
     if(!(value %in% c("logistic","probit","log-log","cauchit"))) stop("possible linkfunctions are: 'logistic','probit','loglog','cloglog' or 'cauchit'")
-  }
-  if(!is.matrix(train)){
-    train<-as.matrix(train)
   }
   setlinkfunc(linkfunction)
   fit<-polr(trainLabels~.,data=train,Hess = TRUE,method = linkfunction)
   fit
 }
 
-# fit<-polr(as.factor(dattrain[,ncol(dattrain)])~.,data=dattrain[,-ncol(dattrain)],Hess = TRUE,method ="logistic")
 # fit<-pomfit(dattrain[,-ncol(dattrain)],as.factor(dattrain[,ncol(dattrain)]),"logistic")
 
 pompredict<-function(model,test){
