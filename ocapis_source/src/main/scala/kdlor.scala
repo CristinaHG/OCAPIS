0f1b8306c3e15346adcb08b1b0944ba96c2747ec
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-07-12 11:50:52 +0200

    changing kParam type to array

--- ocapis_source/src/main/scala/kdlor.scala
+++ ocapis_source/src/main/scala/kdlor.scala
@@ -4,13 +4,13 @@ import breeze.numerics._
 private class kdlor {
 
   // TODO: implement QUICKRBF
-  def computeKernelMatrix(patterns1: DenseMatrix[Double], patterns2:DenseMatrix[Double],kType:String,kParam:Double)={
+  def computeKernelMatrix(patterns1: DenseMatrix[Double], patterns2:DenseMatrix[Double],kType:String,kParam:Array[Double])={
     val Nf1=patterns1.cols
     val Nf2=patterns2.cols
     var KM=DenseMatrix.zeros[Double](Nf1,Nf2)
 
     kType.toUpperCase match {
-      case "GAUSS" | "GAUSSIAN"| "RBF" => (1 to Nf2).foreach(i=> KM(::, i) := exp(-kParam*
+      case "GAUSS" | "GAUSSIAN"| "RBF" => (1 to Nf2).foreach(i=> KM(::, i) := exp(-kParam(1)*
         sum(
           ((patterns1-patterns2(::,i)*DenseMatrix.ones[Double](1,Nf1)) *:* (patterns1-patterns2(::,i)*DenseMatrix.ones[Double](1,Nf1))).t,Axis._0)))
       case "LINEAR" => KM=(patterns1.t * patterns2)/:/patterns1.rows
@@ -18,8 +18,9 @@ private class kdlor {
       case "POLYNOMIAL" | "POLY" => { var multplusbias=(patterns1.t * patterns2)
                                       multplusbias:+=1
                                       KM= (multplusbias/:/patterns1.rows)
-                                      KM:^=kParam
+                                      KM:^=kParam(1)
       }
+      case
       }
     }
   }
