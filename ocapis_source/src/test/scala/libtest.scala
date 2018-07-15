4dcffb0472f547581a7ba275d516dd9e3f8113f4
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-07-15 13:51:22 +0200

    checking elements order in linear and gaussian kernel  matrix

--- ocapis_source/src/test/scala/libtest.scala
+++ ocapis_source/src/test/scala/libtest.scala
@@ -7,7 +7,6 @@ class libtest extends FunSuite{
   val m1=Array(Array(1.0,2.0),Array(3.0,4.0))
   val m2=Array(Array(3.0,4.0),Array(5.0,6.0))
   val gaussianK=Array(3.3546262790251185E-4, 0.1353352832366127, 1.522997974471263E-8, 3.3546262790251185E-4)
-  //val polynomialK=Array()
   val linearK=Array(9.0,13.0,11.0,16.0)
 
   test("length of computed weights should equal labels size"){
@@ -18,8 +17,23 @@ class libtest extends FunSuite{
     assert(kdlor.computeKernelMatrix(m1, m2, "gaussian", Array(1.0)).toArray===gaussianK)
   }
 
+  test("Check elements order in gaussian kernel matrix are correct"){
+    val matrix=kdlor.computeKernelMatrix(m1, m2, "gaussian", Array(1.0))
+    assert(matrix(0,0)==3.3546262790251185E-4)
+    assert(matrix(0,1)==1.522997974471263E-8)
+    assert(matrix(1,0)==0.1353352832366127)
+    assert(matrix(1,1)==3.3546262790251185E-4)
+  }
+
   test("Array from kernel matrix of type linear should equal linealK"){
     assert(kdlor.computeKernelMatrix(m1, m2, "linear", Array(1.0)).toArray===linearK)
   }
 
+  test("Check elements order in linear kernel matrix are correct"){
+    val matrix=kdlor.computeKernelMatrix(m1, m2, "linear", Array(1.0))
+    assert(matrix(0,0)==9.0)
+    assert(matrix(0,1)==11.0)
+    assert(matrix(1,0)==13.0)
+    assert(matrix(1,1)==16.0)
+  }
 }
