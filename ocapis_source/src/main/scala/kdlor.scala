9b49c6683cffa7edb8a9c572d0d68d40b4fcd31c
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-07-16 19:24:46 +0200

    deleting ones matrix multiplication in gaussian kernel

--- ocapis_source/src/main/scala/kdlor.scala
+++ ocapis_source/src/main/scala/kdlor.scala
@@ -3,7 +3,7 @@ import breeze.linalg._
 import breeze.numerics._
 import Numeric._
 
-class kdlor {
+object kdlor {
 
   // TODO: implement QUICKRBF
   def computeKernelMatrix(patterns1: Array[Array[Double]], patterns2: Array[Array[Double]], kType: String, kParam: Array[Double]) = {
@@ -24,56 +24,60 @@ class kdlor {
 
     kType.toUpperCase match {
       case "GAUSS" | "GAUSSIAN" | "RBF" => (0 to Nf2 - 1).foreach(i => {
-        val diff= (data1(::, *) - data2(::, i)*DenseVector.ones[Double](Nf1))^:^2.0
-        KM(::, i) :=exp(-kParam(0)*sum(diff(::, *)).t)})
-        KM
+        val diff = (data1(::, *) - data2(::, i)) ^:^ 2.0
+        KM(::, i) := exp(-kParam(0) * sum(diff(::, *)).t)
+      })
+        KM.toArray
 
       case "LINEAR" => KM = (data1.t * data2) /:/ nrow1.toDouble
-        KM
+        KM.toArray
       case "POLYNOMIAL" | "POLY" => {
-        val multplusbias=(data1.t * data2):+=1.0
-        KM= (multplusbias/:/nrow1.toDouble)^:^kParam(0)
-        KM
+        val multplusbias = (data1.t * data2) :+= 1.0
+        KM = (multplusbias /:/ nrow1.toDouble) ^:^ kParam(0)
+        KM.toArray
       }
       case "SIGMOID" => {
-        if (kParam.length <2) {
+        if (kParam.length < 2) {
           throw sys.error("Sigmoid kernel needs two parameters")
-        }else (0 to Nf2-1).foreach(i=> {
-          KM(::, i):= tanh(data1.t * data2(::,i)*kParam(0)+kParam(1))})
-        KM
+        } else (0 to Nf2 - 1).foreach(i => {
+          KM(::, i) := tanh(data1.t * data2(::, i) * kParam(0) + kParam(1))
+        })
+        KM.toArray
       }
-      case _ =>  throw sys.error("Unknown kernel. Avaiable kernels are: Gauss, Linear, Poly, or Sigmoid.")
+      case _ => throw sys.error("Unknown kernel. Avaiable kernels are: Gauss, Linear, Poly, or Sigmoid.")
     }
-  }
-
-  def assignLabels(projected: DenseMatrix[Double],thresholds:DenseMatrix[Double])={
-    val numClasses=thresholds.cols+1
-    var project2 =tile(projected, 1,numClasses-1)
-    project2=project2 - thresholds.t * DenseMatrix.ones[Double](1,project2.cols)
-    val mapped=project2.mapValues(a=>if(a>0) NaN else a)
-    val maximum=max(mapped(::,*))
-    val rows = mapped(::,*)
-    val indices = maximum.t.map { x =>
-       rows map (a => a.data.indexWhere(_ == x))
-    }
-    var predicted=mapped(::,*).map(a => a.data.indexWhere(_ == maximum))
   }
 }
 
-object kdlor{
+//  def assignLabels(projected: DenseMatrix[Double],thresholds:DenseMatrix[Double])={
+//    val numClasses=thresholds.cols+1
+//    var project2 =tile(projected, 1,numClasses-1)
+////    project2=project2 - thresholds.t * DenseMatrix.ones[Double](1,project2.cols)
+////    val mapped=project2.mapValues(a=>if(a>0) NaN else a)
+////    val maximum=max(mapped(::,*))
+////    val rows = mapped(::,*)
+////    val indices = maximum.t.map { x =>
+////       rows map (a => a.data.indexWhere(_ == x))
+////    }
+////    var predicted=mapped(::,*).map(a => a.data.indexWhere(_ == maximum))
+//  }
+//}
 
+//object kdlor{
+//
 //  def apply(p1: Array[Array[Double]], p2: Array[Array[Double]], kType: String, kParam: Array[Double]): Unit = {
 //    val kd=new kdlor()
 //    kd.computeKernelMatrix(p1,p2,kType,kParam)
 //  }
-
-  def main(args: Array[String]): Unit = {
-    val kd=new kdlor()
-    val d1=DenseMatrix((1.0,2.0), (3.0,4.0))
-    val d2=DenseMatrix((3.0,4.0), (5.0,6.0))
-    kd.assignLabels(d1,d2)
-  }
-}
+//
+//  //def main(args: Array[String]): Unit = {
+//  def assignlabels(projected: DenseMatrix[Double],thresholds:DenseMatrix[Double])={
+//    val kd=new kdlor()
+//    //val d1=DenseMatrix((1.0,2.0), (3.0,4.0))
+//    //val d2=DenseMatrix((3.0,4.0), (5.0,6.0))
+//    kd.assignLabels(projected,thresholds)
+//  }
+//}
 //  override def main(args: Array[Any]): Unit = args(0) match {
 //    case "kdlor"=> kdlor.computeKernelMatrix(args(1), args(2), args(3), args(4))
 //  }
