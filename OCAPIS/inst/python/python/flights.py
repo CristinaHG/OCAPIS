11fa4f2f8bb6fed25992e42cf208492208c06145
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-07-17 12:45:31 +0200

    removing example files from /inst/python/python

--- OCAPIS/inst/python/python/flights.py
+++
@@ -1,8 +0,0 @@
-import pandas
-
-def read_flights(file):
-  flights = pandas.read_csv(file)
-  flights = flights[flights['dest'] == "ORD"]
-  flights = flights[['carrier', 'dep_delay', 'arr_delay']]
-  flights = flights.dropna()
-  return flights
