73b6791d1553da874c81ad0d3a881fcde54254d2
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-07-17 09:33:26 +0200

    adding private instance fields in kdlor

--- ocapis_source/src/main/scala/kdlor.scala
+++ ocapis_source/src/main/scala/kdlor.scala
@@ -3,8 +3,12 @@ import breeze.linalg._
 import breeze.numerics._
 import Numeric._
 
-object kdlor {
-
+class kdlor {
+ private var projection=DenseMatrix.zeros[Double]
+ private var thresholds=DenseMatrix.zeros[Double]
+ private var parameters=Array()
+ private var kerneltype:String="linear"
+  
   // TODO: implement QUICKRBF
   def computeKernelMatrix(patterns1: Array[Array[Double]], patterns2: Array[Array[Double]], kType: String, kParam: Array[Double]) = {
     //parse data from R format to Breeze DenseMatrix
@@ -27,14 +31,14 @@ object kdlor {
         val diff = (data1(::, *) - data2(::, i)) ^:^ 2.0
         KM(::, i) := exp(-kParam(0) * sum(diff(::, *)).t)
       })
-        KM
+        KM.toArray
 
       case "LINEAR" => KM = (data1.t * data2) /:/ nrow1.toDouble
-        KM
+        KM.toArray
       case "POLYNOMIAL" | "POLY" => {
         val multplusbias = (data1.t * data2) :+= 1.0
         KM = (multplusbias /:/ nrow1.toDouble) ^:^ kParam(0)
-        KM
+        KM.toArray
       }
       case "SIGMOID" => {
         if (kParam.length < 2) {
@@ -42,16 +46,16 @@ object kdlor {
         } else (0 to Nf2 - 1).foreach(i => {
           KM(::, i) := tanh(data1.t * data2(::, i) * kParam(0) + kParam(1))
         })
-        KM
+        KM.toArray
       }
       case _ => throw sys.error("Unknown kernel. Avaiable kernels are: Gauss, Linear, Poly, or Sigmoid.")
     }
   }
-}
 
-//  def assignLabels(projected: DenseMatrix[Double],thresholds:DenseMatrix[Double])={
-//    val numClasses=thresholds.cols+1
-//    var project2 =tile(projected, 1,numClasses-1)
+
+  def assignLabels(projected: DenseMatrix[Double],thresholds:DenseMatrix[Double])={
+    val numClasses=thresholds.cols+1
+    var project2 =tile(projected, 1,numClasses-1)
 ////    project2=project2 - thresholds.t * DenseMatrix.ones[Double](1,project2.cols)
 ////    val mapped=project2.mapValues(a=>if(a>0) NaN else a)
 ////    val maximum=max(mapped(::,*))
@@ -60,24 +64,24 @@ object kdlor {
 ////       rows map (a => a.data.indexWhere(_ == x))
 ////    }
 ////    var predicted=mapped(::,*).map(a => a.data.indexWhere(_ == maximum))
-//  }
-//}
+  }
+}
 
-//object kdlor{
-//
+object kdlor{
+
 //  def apply(p1: Array[Array[Double]], p2: Array[Array[Double]], kType: String, kParam: Array[Double]): Unit = {
 //    val kd=new kdlor()
 //    kd.computeKernelMatrix(p1,p2,kType,kParam)
 //  }
-//
-//  //def main(args: Array[String]): Unit = {
+////
+////  //def main(args: Array[String]): Unit = {
 //  def assignlabels(projected: DenseMatrix[Double],thresholds:DenseMatrix[Double])={
 //    val kd=new kdlor()
 //    //val d1=DenseMatrix((1.0,2.0), (3.0,4.0))
 //    //val d2=DenseMatrix((3.0,4.0), (5.0,6.0))
 //    kd.assignLabels(projected,thresholds)
 //  }
-//}
+}
 //  override def main(args: Array[Any]): Unit = args(0) match {
 //    case "kdlor"=> kdlor.computeKernelMatrix(args(1), args(2), args(3), args(4))
 //  }
