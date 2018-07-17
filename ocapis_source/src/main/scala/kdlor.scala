c15ba66b3c1f189dadfbc4265711caad46bccc00
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-07-17 12:38:14 +0200

    changing computeKernelMatrix data tipes and return type

--- ocapis_source/src/main/scala/kdlor.scala
+++ ocapis_source/src/main/scala/kdlor.scala
@@ -12,20 +12,21 @@ class kdlor {
  private var optimizationMethod="quadprog"
 
   // TODO: implement QUICKRBF
-  def computeKernelMatrix(patterns1: Array[Array[Double]], patterns2: Array[Array[Double]], kType: String, kParam: Array[Double]) = {
+  def computeKernelMatrix(data1: DenseMatrix[Double], data2: DenseMatrix[Double], kType: String, kParam: Array[Double]):DenseMatrix[Double] = {
     //parse data from R format to Breeze DenseMatrix
-    val ncol1 = patterns1.length
-    val nrow1 = patterns1.take(2).map(a => a.length).max
-    val ncol2 = patterns2.length
-    val nrow2 = patterns2.take(2).map(a => a.length).max
-
-    val dat1 = new DenseMatrix(nrow1, ncol1, patterns1.flatten)
-    val data1 = dat1.t
-    val dat2 = new DenseMatrix(nrow2, ncol2, patterns2.flatten)
-    val data2 = dat2.t
+//    val ncol1 = patterns1.length
+//    val nrow1 = patterns1.take(2).map(a => a.length).max
+//    val ncol2 = patterns2.length
+//    val nrow2 = patterns2.take(2).map(a => a.length).max
+//
+//    val dat1 = new DenseMatrix(nrow1, ncol1, patterns1.flatten)
+//    val data1 = dat1.t
+//    val dat2 = new DenseMatrix(nrow2, ncol2, patterns2.flatten)
+//    val data2 = dat2.t
 
     val Nf1 = data1.cols
     val Nf2 = data2.cols
+    val nrow1 =data1.rows
     var KM = DenseMatrix.zeros[Double](Nf1, Nf2)
 
     kType.toUpperCase match {
@@ -33,14 +34,14 @@ class kdlor {
         val diff = (data1(::, *) - data2(::, i)) ^:^ 2.0
         KM(::, i) := exp(-kParam(0) * sum(diff(::, *)).t)
       })
-        KM.toArray
+        KM
 
       case "LINEAR" => KM = (data1.t * data2) /:/ nrow1.toDouble
-        KM.toArray
+        KM
       case "POLYNOMIAL" | "POLY" => {
         val multplusbias = (data1.t * data2) :+= 1.0
         KM = (multplusbias /:/ nrow1.toDouble) ^:^ kParam(0)
-        KM.toArray
+        KM
       }
       case "SIGMOID" => {
         if (kParam.length < 2) {
@@ -48,9 +49,11 @@ class kdlor {
         } else (0 to Nf2 - 1).foreach(i => {
           KM(::, i) := tanh(data1.t * data2(::, i) * kParam(0) + kParam(1))
         })
-        KM.toArray
+        KM
       }
-      case _ => throw sys.error("Unknown kernel. Avaiable kernels are: Gauss, Linear, Poly, or Sigmoid.")
+      case _ =>  KM
+        // throw sys.error("Unknown kernel. Avaiable kernels are: Gauss, Linear, Poly, or Sigmoid.")
+
     }
   }
 
