0834cbc336e28f324908ef8beed795f2baf03ffe
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-07-23 19:41:32 +0200

    writing test for projectionTrain matrix in kdlor

--- ocapis_source/src/test/scala/kdlortest.scala
+++ ocapis_source/src/test/scala/kdlortest.scala
@@ -16,5 +16,20 @@ class kdlortest extends FunSuite{
   val labs=casted.map(u=>u(u.length-1).toInt)
   val trainDataMatrix=casted.map(u=>u.take(u.length-1))
   print(casted(0)(0))
+  val kd=new kdlor
+  val kt="rbf"
+  val params=Array(1.0,2.0)
+  val optim="qp"
+  val fitted=kd.train(trainDataMatrix,labs,kt,params,optim)
+  val projectedTrainfile=io.Source.fromFile(getClass.getResource("/projectedTrain").getPath)
+  var projectedTrainM=ArrayBuffer[Array[Double]]()
+  for (line <- projectedTrainfile.getLines.filterNot(s=>s.startsWith("#"))) {
+    var item=line.split(" ").map(_.trim.toDouble)
+    projectedTrainM+=item
+  }
+  val projectedTrainMatrix=rows.toArray
+  test("trained method should provide projectedTrainToMatrix data equals to projectedTrainMatrix"){
+  assert(fitted(0)==projectedTrainMatrix)
+  }
 
 }
