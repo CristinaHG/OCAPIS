55f626721263db89c19383b618c1988e402807ed
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-08-06 11:50:53 +0200

    implementing rectangular kernel

--- ocapis_source/src/main/scala/cristinahg/ocapis/wknn.scala
+++ ocapis_source/src/main/scala/cristinahg/ocapis/wknn.scala
@@ -1,9 +1,22 @@
 package cristinahg.ocapis
 
 import breeze.linalg.{*, DenseMatrix}
+import breeze.numerics.abs
 import breeze.stats.stddev
 
 class wknn {
+
+  def computeWeights(kernelType:String, distances:Array[Double]):Array[Double]={
+    kernelType.toUpperCase match {
+      case "rectangular" => distances.map(d=>{
+        if(abs(d)<=1){
+          (1/2)*1
+        }else (1/2)*0
+      })
+      case   
+    }
+  }
+
   def wknn(trainData:Array[Array[Double]],trainLabels:Array[Int],testData:Array[Array[Double]],k:Int,q:Double,kernelType:String): Unit ={
     val ncoltrain = trainData.length
     val nrowtrain = trainData.take(2).map(a => a.length).max
@@ -14,6 +27,6 @@ class wknn {
     val datTest = new DenseMatrix(nrowtest,ncoltest, testData.flatten)
 
     val standarized=stddev(datTrain(::,*))
-    
+
   }
 }
