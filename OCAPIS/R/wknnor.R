dbac7874e50891fe170cfb2f62ffa74d336a6a8d
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-08-19 12:16:55 +0200

    adding checkups for wknnor params

--- OCAPIS/R/wknnor.R
+++ OCAPIS/R/wknnor.R
@@ -2,11 +2,11 @@
 #'
 #' Predicts labels for test data with ordinal nature or monotonic constraints using an adaptation
 #' of wknn.
-#' @param trainData Training data of numeric type without labels.
-#' @param trainLabels A vector of numeric tags for each instance of training data.
-#' @param testData Test data of numeric type.
+#' @param traindata Training data of numeric type without labels.
+#' @param trainlabels A vector of numeric tags for each instance of training data.
+#' @param testdata Test data of numeric type.
 #' @param q Minkowski distance param. Use q=1 for Manhattan distance and q=2  for Euclidean distance.
-#' @param kernelType Kernel used to compute neighbors weights. Avaiable kernels are: rectangular, triangular,epanechnikov,
+#' @param kerneltype Kernel used to compute neighbors weights. Avaiable kernels are: rectangular, triangular,epanechnikov,
 #'biweight, triweight, cosine, gauss and inversion.
 #' @param monotonicity Boolean param specifying whether data is monotone or not.
 #' @return Predicted labels for test data.
@@ -19,7 +19,27 @@
 #' testlabels<-wknnor(traindata,trainlabels,testdata,5,2,"rectangular",FALSE)
 #'
 
-wknnor<-function(trainData,trainLabels,testData,k,q,kernelType,monotonicity){
+wknnor<-function(traindata,trainlabels,testdata,k,q,kerneltype,monotonicity){
+  if(nargs()<3) stop("Train Data and labels and TestData must be provided.\n")
+
+  if (length(trainlabels)!= nrow(traindata)){
+    stop('Number of train patterns and targets should be the same.\n');
+  }
+  if(k<0){
+    stop("Number of neighbors must be positive.")
+  }
+  if(!tolower(kernelType) %in% c("rectangular","triangular","epanechnikov","biweight","triweight","cosine",
+                                 "gauss","inversion")){
+    stop("Unknown kernel. Avaiable kernels are: rectangular, triangular,epanechnikov,
+          biweight, triweight, cosine, gauss and inversion.")
+  }
+
+  if(!typeof(monotonicity)=="logical"){
+    stop("monotonicity param must be either TRUE or FALSE")
+  }
+  if (!is.matrix(traindata)){traindata=as.matrix(traindata)}
+  if (!is.matrix(testdata)){testdata=as.matrix(testdata)}
+
 
 }
 
