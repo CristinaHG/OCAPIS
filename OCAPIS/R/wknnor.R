6bad9134cbbf493f813812d2184c009ac76a8072
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-08-19 11:58:18 +0200

    adding doc for wknn

--- OCAPIS/R/wknnor.R
+++ OCAPIS/R/wknnor.R
@@ -1,17 +1,26 @@
 #' K-nearest neighbors for ordinal and monotonic data
 #'
-#' train data must be the data without labels. Labels should be provided in trainLabels
-#' @param x Training data of numeric type without labels.
-#' @param y A vector of numeric tags for each instance of training data.
-#' @param weights A boolean indicating whether weights per instance are used.
-#' @param cost numeric value indicating the cost parameter to train the SVM.
-#' @param gamma numeric value indicating the gamma parameter to train the SVM.
-#' @return A matrix of 1xn svm trained with weights models.
+#' Predicts labels for test data with ordinal nature or monotonic constraints using an adaptation
+#' of wknn.
+#' @param trainData Training data of numeric type without labels.
+#' @param trainLabels A vector of numeric tags for each instance of training data.
+#' @param testData Test data of numeric type.
+#' @param q Minkowski distance param.Use q=1 for Manhattan distance and q=2  for Euclidean distance.
+#' @param kernelType Kernel used to compute neighbors weights. Avaiable kernels are: rectangular, triangular,epanechnikov,
+#'biweight, triweight, cosine, gauss and inversion.
+#' @param monotonicity Boolean param specifying whether data is monotone or not.
+#' @return Predicted labels for test data.
 #' @examples
 #' dattrain<-read.csv("train_balance-scale.0", sep=" ")
-#' modelstrain<-svmofit(dattrain[,-ncol(dattrain)],dattrain[,ncol(dattrain)],TRUE,1,1)
+#' traindata=dattrain[,-ncol(dattrain)]
+#' trainlabels=dattrain[,ncol(dattrain)]
+#' testdata<-read.csv("test_balance-scale.0", sep=" ")
+#' testdata<-testdata[,-ncol(testdata)]
+#' testlabels<-svmofit(traindata,trainlabels,testdata,5,2,"rectangular",FALSE)
 #'
 
 wknnor<-function(trainData,trainLabels,testData,k,q,kernelType,monotonicity){
 
 }
+
+
