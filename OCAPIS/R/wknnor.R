d22b25ef1678c72f9048c0bc9b6801d44b29c76c
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-08-19 12:01:37 +0200

    correcting documentation for wknnor

--- OCAPIS/R/wknnor.R
+++ OCAPIS/R/wknnor.R
@@ -5,7 +5,7 @@
 #' @param trainData Training data of numeric type without labels.
 #' @param trainLabels A vector of numeric tags for each instance of training data.
 #' @param testData Test data of numeric type.
-#' @param q Minkowski distance param.Use q=1 for Manhattan distance and q=2  for Euclidean distance.
+#' @param q Minkowski distance param. Use q=1 for Manhattan distance and q=2  for Euclidean distance.
 #' @param kernelType Kernel used to compute neighbors weights. Avaiable kernels are: rectangular, triangular,epanechnikov,
 #'biweight, triweight, cosine, gauss and inversion.
 #' @param monotonicity Boolean param specifying whether data is monotone or not.
@@ -16,7 +16,7 @@
 #' trainlabels=dattrain[,ncol(dattrain)]
 #' testdata<-read.csv("test_balance-scale.0", sep=" ")
 #' testdata<-testdata[,-ncol(testdata)]
-#' testlabels<-svmofit(traindata,trainlabels,testdata,5,2,"rectangular",FALSE)
+#' testlabels<-wknnor(traindata,trainlabels,testdata,5,2,"rectangular",FALSE)
 #'
 
 wknnor<-function(trainData,trainLabels,testData,k,q,kernelType,monotonicity){
