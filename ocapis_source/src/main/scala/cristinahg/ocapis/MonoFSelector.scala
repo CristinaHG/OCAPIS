e4e75c51c5ae0cf67c8e3779b6ea023ed09434f1
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-08-22 12:30:39 +0200

    creating method for computing fuzzy relation matrix

--- ocapis_source/src/main/scala/cristinahg/ocapis/MonoFSelector.scala
+++ ocapis_source/src/main/scala/cristinahg/ocapis/MonoFSelector.scala
@@ -2,18 +2,22 @@ package cristinahg.ocapis
 
 import breeze.linalg.{*, DenseMatrix}
 import breeze.numerics.exp
+import breeze.linalg.{*, DenseMatrix, DenseVector}
+
 
 class MonoFSelector {
 
-  private def computeRelation(k: Int, xiVal:Double, xjVal: Double):
+  private def computeRelation(k: Int, xiVal:Array[Double], xjVal: Array[Double],featureIndex:Int):
   Double={
-    1/(1+ exp(k*(xiVal-xjVal)))
+    1/(1+ exp(k*(xiVal(featureIndex)-xjVal(featureIndex))))
   }
 
-  private def fuzzySet(instanceIndex:Int, instanceRelations: Array[Array[Double]], dataInstances: DenseMatrix[Double]):
-  Double={
-
+  private def fuzzyMat(datTrain:DenseMatrix[Double],k: Int,featureIndex:Int):DenseMatrix[Double]={
+    datTrain(*,::).map(f=>{
+      datTrain(*,::).map(r=> computeRelation(k,f.toArray,r.toArray,featureIndex))
+    }).toDenseMatrix
   }
+
 
   def MonoFSelector(trainData: Array[Array[Double]], trainLabels: Array[Int],k: Int, nSelected: Int):Array[Int]={
     val ncoltrain = trainData.length
@@ -22,11 +26,25 @@ class MonoFSelector {
     val datTrain = datTr.t
 
     val nfeatures=datTrain.cols
-    val relationsMat=(0 until nfeatures).map(f=>{
-      datTrain(*,::).map(r=>{
-        r mapValues(i=>computeRelation(k,))
-      })
-    })
+    val fuzzy0=fuzzySet(datTrain,k,0)
 
+    Array(0)
+
+
+
+//      (0 until nfeatures).map(f=>{
+//      datTrain(*,::).map(r=>{
+//        r mapValues(i=>computeRelation(k,))
+//      })
+//    })
+
+  }
+}
+
+object MonoFSelector {
+  val mfs=new MonoFSelector
+  val trainDat=Array(Array(1.0,2.0,3.0), Array(5.0,4.0,2.0))
+  def main(args: Array[String]): Unit = {
+    mfs.MonoFSelector(trainDat,Array(1,2),2,3)
   }
 }
