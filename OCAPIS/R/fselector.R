7f1e5522ab369605075e4709538dc707525f70dc
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-08-26 20:23:19 +0200

    adding doc for Fselector

--- OCAPIS/R/fselector.R
+++ OCAPIS/R/fselector.R
@@ -1,3 +1,20 @@
+#' Feature Selection for Monotonic Classification
+#'
+#' Selects the N most relevant features from ordinal and monotonic data, based on mRMR criterion.
+#'
+#' @param traindata Training data of numeric type without labels.
+#' @param trainlabels A vector of numeric tags for each instance of training data.
+#' @param k positive constant for logit function. If large, fuzzy ordinal set is understood as slightly
+#' larger. If small, is understood as significantly larger.
+#' @param beta Regulation param for relative importance of MI between features and decision.
+#' @param nselected Number of features to select.
+#' @return nselected most important features.
+#' @examples
+#' dattrain<-read.csv("train_balance-scale.0", sep=" ")
+#' traindata=dattrain[,-ncol(dattrain)]
+#' selected<-fselector(traindata,trainlabels,2,2,2)
+#'
+#'
 fselector<-function(traindata,trainlabels,k,beta,nselected){
   if(nargs()<3) stop("Train Data and labels and number of features to select must be provided.\n")
 
