3017bde8810b3ba2a7b9a180ff9cae6370c7463e
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-08-28 18:03:18 +0200

    making neighbor and neighborWeights classes comparable

--- ocapis_source/src/main/scala/cristinahg/ocapis/NeighborWeight.scala
+++ ocapis_source/src/main/scala/cristinahg/ocapis/NeighborWeight.scala
@@ -1,29 +1,13 @@
 package cristinahg.ocapis
 
-class NeighborWeight( var weight:Double = .0, var index:Int = -1) extends Comparable[_] {
+class NeighborWeight( var weight:Double = .0, var index:Int = -1) extends Ordered[Any] {
 
-  def this(_ind: Int, _weight: Double) {
-    this()
-    weight = _weight
-    index = _ind
-  }
-
-  def getWeight: Double = weight
-
-  def setWeight(_weight: Double): Unit = {
-    weight = _weight
-  }
-
-  override def compareTo(o: Any): Int = {
+  override def compare(o: Any): Int = {
     val n = o.asInstanceOf[NeighborWeight]
-    if (this.getWeight > n.getWeight) 1
-    else if (this.getWeight < n.getWeight) -(1)
+    if (this.weight > n.weight) 1
+    else if (this.weight < n.weight) -(1)
     else 0
   }
-
-  def setIndex(_ind: Int): Unit = {
-    index = _ind
-  }
-
-  def getIndex: Int = index
 }
+
+
