6f71b393d4c9f2081faccb032047fcfd0f0e8236
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-10-04 18:03:59 +0200

    generating doc for load_libsvm

--- OCAPIS/R/util.R
+++ OCAPIS/R/util.R
@@ -1,3 +1,10 @@
+
+#' Function to look for libsvm-weights binaries on the system
+#'
+#' The user must install libsvm-weights as explained in the doc. Then this function is used to find the compiled library
+#' on the system.
+#' @return the object containing the library bin, if found. Else throws an error.
+
 load_libsvm<-function(){
   libdir<-file.path("/usr/lib","libsvm-weights-3.17","python")
   if(dir.exists(libdir)){
