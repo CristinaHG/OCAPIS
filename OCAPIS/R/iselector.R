7a63ce2705c6a54c8a87d885d36efb0c87773389
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-10-05 11:32:38 +0200

    updating doc for iselector

--- OCAPIS/R/iselector.R
+++ OCAPIS/R/iselector.R
@@ -9,7 +9,7 @@
 #' @param kEdition Maximum number of nearest neighborgs to consider.
 #' @return A reduced dataset with the selected instances and its labels.
 #' @examples
-#' dattrain<-read.table("train_balance-scale.0", sep=" ")
+#' load("./data/train_balance-scale.rda")
 #' trainlabels<-dattrain[,ncol(dattrain)]
 #' traindata=dattrain[,-ncol(dattrain)]
 #' selected<-iselector(traindata,trainlabels,0.01,0.01,5)
