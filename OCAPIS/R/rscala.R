4ffc8792df09e5fef21fd70e739b3a2ac876feab
CristinaHG <mrcrstnherediagmez@gmail.com>
2018-10-05 17:29:41 +0200

    adding documentation for rscala objects

---
+++ OCAPIS/R/rscala.R
@@ -0,0 +1,23 @@
+#' rscala bridge name
+#'
+#' Object from rscala package, it returns the name of the bridge between r and Scala
+#'
+#' @return The Name of the bridge between R and Scala. By default is 's'
+"rscalaBridgeName"
+
+
+#' rscala bridge owner
+#'
+#' Object from rscala package, is a boolean pointing if the embeded scala bridge in the package has owner
+#'
+#' @return TRUE if embeded scala bridge in the package has owner, else FALSE
+"rscalaBridgeOwner"
+
+
+#' Scala interpreter instance
+#'
+#' Scala interpreter instance named 's' by default. Name can be changed using 'assign.name'
+#'
+#' @return The avaiable Scala instance
+"s"
+
